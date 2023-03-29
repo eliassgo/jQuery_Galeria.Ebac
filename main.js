e78f5360262e68c3af3057840e130b16cf3d6156
Elias Gomes <eliasgo221@gmail.com>
2023-03-29 00:33:38 -0300

    modificações

--- main.js
+++ main.js
@@ -8,20 +8,20 @@ $(document).ready(function() { //quando o documento estiver pronto execute a fun
     })
 
     $('form').on('submit', function(e) { //ao clicar no button do formulário executar essa funcção
-        e.preventeDefault();
+        e.preventDefault()
         const enderecoDaNovaImagem = $('#endereco-imagem-nova').val(); //criando constante com id do html
         const novoItem = $('<li style="display: none"></li>'); //criando constante
         $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);//colocar img dentro do novo item
         $(`
             <div class="overlay-imagem-link">
-                <a href="${enderecoDaNovaImagem}" target="_blank" title="ver imagem em tamanho real"
+                <a href="${enderecoDaNovaImagem}" target="_blank" title="ver imagem em tamanho real">
                 Ver imagem em tamanho real
                 </a>
             </div>
         `).appendTo(novoItem);//colocando dentro do novo item
         $(novoItem).appendTo('ul');//colocando novo item dentro do ul
         $(novoItem).fadeIn();
-        $('endereço-imagem-nova').val('')
+        $('#endereço-imagem-nova').val('')
     })
 })
 //JQUERY funciona através do () e do $, $()entre parênteses fica o seletor
